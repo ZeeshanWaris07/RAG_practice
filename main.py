c8e647ede58c5700f92a2e45c62544fd694b70a0
Zeeshan Waris <zeeshanwaris3345@gmail.com>
2026-07-24 17:01:19 +0500

    Initialized the repository and also imported all necessary packaged related to the project

---
+++ main.py
@@ -0,0 +1,16 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_ollama import ChatOllama
+
+
+loader = PyPDFLoader('Introduction-to-AI-and-Basic-Concepts.pdf')
+documents = loader.load()
+
+
+
+print(len(documents))
