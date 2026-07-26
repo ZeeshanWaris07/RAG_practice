1005e7614794f210facd1653a0e2575caa0496b9
Zeeshan Waris <zeeshanwaris3345@gmail.com>
2026-07-26 14:59:32 +0500

    Completed my work on retreiver which retreives relevant chunks from the vector store

--- main.py
+++ main.py
@@ -11,6 +11,29 @@ from langchain_ollama import ChatOllama
 loader = PyPDFLoader('data/Introduction-to-AI-and-Basic-Concepts.pdf')
 documents = loader.load()
 
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 500,
+    chunk_overlap  = 50
+)
 
+chunks = text_splitter.split_documents(documents)
 
-print(len(documents))
+embeddings = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-small-en-v1.5"
+)
+
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    persist_directory='./chroma_db'
+)
+
+retreiver = vector_db.as_retriever(
+    search_kwargs = {"k":3}
+)
+
+question = "What is Gradient Descent?"
+
+chunks = retreiver.invoke(question)
+
+print(chunks)
